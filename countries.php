df84194f3c818b0b10e6270dde7baa2da1ab483f
Igor <igoostro@gmail.com>
2014-06-28 23:52:56 +0200

    strona countries, zabawa z baza, poprawka po blad2, v3

--- countries.php
+++ countries.php
@@ -20,9 +20,7 @@
         
         //Zbieranie danych z tabeli country
         $query1 = "SELECT continent, name, population, code FROM country ORDER BY continent ASC, name ASC";
-        $result1 = pg_query($connect, $query1);
-        if(!$result1)
-            echo "Błąd 1\n";
+        $result1 = pg_query($connect, $query1) or die("Nie mozna wykonac zapytania: $query1\n");
         ?>
         
         <table>
@@ -32,9 +30,8 @@
         while ($row1 = pg_fetch_row($result1)){
             //Zbieranie danych z tabeli countryLanguage
             $query2 = "SELECT language, percentage FROM countryLanguage WHERE isofficial=true AND countrycode='$row1[3]' ORDER BY population DESC";
-            $result2 = pg_query($connect, $query2);
-            if(!$result2)
-                echo "Błąd 2\n";
+            $result2 = pg_query($connect, $query2) or die("Nie mozna wykonac zapytania: $query2\n");
+            
             while ($row2 = pg_fetch_row($result2)){
                 echo "<tr><td>$row1[0]</td><td>$row1[1]</td><td>$row2[0]</td><td>$row1[2]</td><td>$row2[1]</td><td>($row1[2]*$row2[1]/100)</td><td>Details</td></tr>";
                 //TO DO zaokraglenia!! 
