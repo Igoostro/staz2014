2a77762dc959b6754c94bf1c0fd53253cf63ef5b
Igor <igoostro@gmail.com>
2014-06-28 21:32:42 +0200

    Zaczyna sie, 28-06-14, 21:32

--- index.php
+++ index.php
@@ -2,15 +2,16 @@
 <head>
     <meta charset="utf-8" />
     <meta author="Igor Ostrowski" />
+    <link rel="Stylesheet" type="text/css" href="style.css" />
+    
     <title>Countries and Languages</title>
 </head>
     <body>
-        <div id="main">
+        <div id="top">
             <h1>Countries and Languages</h1>
         </div>
-        <div> 
-            <a href="/countries">Countries</a>
-            <a href="/groupedCountries">Languages by Numbers</a>
-        </div>
+        <?php
+        include 'links.php';
+        ?>
     </body>
 </html>
---
+++ links.php
@@ -0,0 +1,4 @@
+<div>
+    <a href="/countries">Countries</a>
+    <a href="/groupedCountries">Languages by Numbers</a>
+</div>
