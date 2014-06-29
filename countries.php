4a493abb9fd08ff742af48ff3a503b64144f8faf
Igor <igoostro@gmail.com>
2014-06-29 21:14:50 +0200

    Pierwsza tabela done

--- countries.php
+++ countries.php
@@ -10,6 +10,7 @@
         <div id="top">
             <h1>Official languages of countries</h1>
         </div>
+        
         <?php
         //Dodanie linkow
         include 'links.php';
@@ -24,34 +25,30 @@
         ?>
         
         <table>
-            <tr><td>Continent</td><td>Country</td><td>Language</td><td>Population</td><td>Official Language Use %</td><td>Official Language Population Use</td><td>&nbsp;</td></tr>
+            <thead>
+                <tr><th>Continent</th><th>Country</th><th>Language</th><th>Population</th><th>Official Language Use %</th><th>Official Language Population Use</th><th style="border: none">&nbsp;</th></tr>
+            </thead>
+            <tbody>
             
-        <?php
-        //testy start
-        
-        //$query3 = "SELECT * FROM countryLanguage";
-        //$result3 = pg_query($connect, $query3) or die("Nie mozna wykonac zapytania: $query3\n");
-        //while ($row3 = pg_fetch_row($result3)){
-        //    echo "$row3[0] $row3[1] $row3[2] $row3[3]";
-        //}
-        //testy stop
-        while ($row1 = pg_fetch_row($result1)){
-            //Zbieranie danych z tabeli countryLanguage
-            $query2 = "SELECT language, percentage FROM countryLanguage WHERE countrycode='$row1[3]' AND isofficial=true ORDER BY percentage DESC";
-            $result2 = pg_query($connect, $query2) or die("Nie mozna wykonac zapytania: $query2\n");
+                <?php
+                while ($row1 = pg_fetch_row($result1)){
+                    //Zbieranie danych z tabeli countryLanguage
+                    $query2 = "SELECT language, percentage FROM countryLanguage WHERE countrycode='$row1[3]' AND isofficial=true ORDER BY percentage DESC";
+                    $result2 = pg_query($connect, $query2) or die("Nie mozna wykonac zapytania: $query2\n");
             
-            while ($row2 = pg_fetch_row($result2)){
-                $temportary = $row1[2]*$row2[1]/100;
-                echo "<tr><td>$row1[0]</td><td>$row1[1]</td><td>$row2[0]</td><td>$row1[2]</td><td>$row2[1]</td><td>$temportary</td><td>Details</td></tr>";
-                //TO DO zaokraglenia!! 
-            }
-        }
+                    while ($row2 = pg_fetch_row($result2)){
+                        $offLanUsePer = round($row2[1], 2);
+                        $offLanPopUse = floor($row1[2]*$row2[1]/100);
+                        echo "<tr><td>$row1[0]</td><td>$row1[1]</td><td>$row2[0]</td><td class='numbers'>$row1[2]</td><td class='numbers'>$offLanUsePer</td><td class='numbers'>$offLanPopUse</td><td id='details'>Details</td></tr>";
+                        //TO DO zaokraglenia!! 
+                    }
+                }
         
-        pg_free_result($result1);
-        pg_free_result($result2);
-        pg_close($connect);
-
-        ?>
+                pg_free_result($result1);
+                pg_free_result($result2);
+                pg_close($connect);
+                ?>
+            </tbody>
         </table>
     </body>
 </html>
